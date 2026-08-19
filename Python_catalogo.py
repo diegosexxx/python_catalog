aa298a6256e1006d40a069a14064f30d62a12a94
Diego Urrutia <diegourrutia@ufm.edu>
2026-08-19 14:02:31 -0600

    agrego funciones ver todos

--- Python_catalogo.py
+++ Python_catalogo.py
@@ -1,26 +1,34 @@
 
 
-
-
-print("Catalogo Albums")
-
 Catalogo_album = {
-        "Blonde": {"Artista:": "Frank Ocean" , "Canciones": "17", "Puntuacion del album": "16" }}
+                "Blonde": {"Artista:": "Frank Ocean" , "Canciones": "17", "Puntuacion del album": "16" }}
+while True:
+        print("Catalogo Albums")
 
-album = input("Agregue el nombre del Album: ")
-artista = input("Agregue el nombre del artista: ")
-canciones = input("Cantidad de canciones: ")
-puntuacion = input("Puntuacion del album: ")
+       
 
-Catalogo_album[album] = {
-        "Artista": artista,
-        "Canciones": canciones,
-        "Puntuacion del album": puntuacion
-    }
+        Catalogo_menu = input(""" Seleccione una opcion:
+        1. Agregar album
+        2. Ver catalogo completo
+        """
+        )
 
-print  (Catalogo_album)
-    
+        if Catalogo_menu == "1":
 
+                album = input("Agregue el nombre del Album: ")
+                artista = input("Agregue el nombre del artista: ")
+                canciones = input("Cantidad de canciones: ")
+                puntuacion = input("Puntuacion del album: ")
+
+                Catalogo_album[album] = {
+                        "Artista": artista,
+                        "Canciones": canciones,
+                        "Puntuacion del album": puntuacion
+                }
+
+        if Catalogo_menu == "2":
+                print(Catalogo_album)
+                
 
 
 
